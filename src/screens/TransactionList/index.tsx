fcbbb5b95cc06ff672336e2a28e03ef01fad218c
ryan <rukmanary@gmail.com>
2024-11-17 06:13:39 +0700

    add palceholder screens

---
+++ src/screens/TransactionList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, ScrollView, Text } from 'react-native';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { COLORS } from '../../themes';
+
+const TransactionList = ({ navigation }: { navigation: any }) => (
+  <ScrollView
+    style={styles.container}
+    contentContainerStyle={styles.contentStyle}>
+    <View>
+      <Text>Transaction List</Text>
+    </View>
+  </ScrollView>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: COLORS.whiteSmoke },
+  contentStyle: { flex: 1, padding: 16 },
+});
+
+export default TransactionList;
