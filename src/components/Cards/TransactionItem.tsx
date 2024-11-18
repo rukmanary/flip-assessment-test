a929a7df7551fc684c0263a99c263539927ade59
ryan <rukmanary@gmail.com>
2024-11-18 15:50:18 +0700

    use BankTransferRoute and modify passing param

--- src/components/Cards/TransactionItem.tsx
+++ src/components/Cards/TransactionItem.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Icon } from '../../assets';
 import { COLORS } from '../../themes';
-import { TransferStatus } from '..';
+import { BankTransferRoute, TransferStatus } from '..';
 import { DetailTransactionData, Status } from '../../types';
 import colors from '../../themes/colors';
 import { useNavigation } from '../../hooks';
@@ -15,7 +14,7 @@ const TransactionItem = ({ item }: TransactionItem) => {
   const { navigateToDetails } = useNavigation();
   return (
     <TouchableOpacity
-      onPress={() => navigateToDetails(item.id)}
+      onPress={() => navigateToDetails(item)}
       activeOpacity={0.7}
       style={styles.cardContainer}>
       <View
@@ -31,11 +30,10 @@ const TransactionItem = ({ item }: TransactionItem) => {
       />
       <View style={styles.contentContainer}>
         <View>
-          <View style={styles.row}>
-            <Text style={styles.textBold}>{item.sender_bank || '-'}</Text>
-            <Icon.ArrowRight />
-            <Text style={styles.textBold}>{item.beneficiary_bank || '-'}</Text>
-          </View>
+          <BankTransferRoute
+            from={item.sender_bank}
+            to={item.beneficiary_bank}
+          />
           <Text style={styles.regularText}>{item.beneficiary_name || ''}</Text>
           <View style={styles.row}>
             <Text>{item.amount || ''}</Text>
@@ -73,7 +71,6 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 8,
   },
   row: { flexDirection: 'row', alignItems: 'center' },
-  textBold: { fontWeight: 'bold' },
   regularText: { fontWeight: '400', lineHeight: 28 },
   circle: {
     backgroundColor: colors.black,
