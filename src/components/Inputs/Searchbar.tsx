5ae96b9e687ddc594984ea9ff99f3f4bb39721fc
ryan <rukmanary@gmail.com>
2024-11-18 15:08:22 +0700

    add search bar

---
+++ src/components/Inputs/Searchbar.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { COLORS } from '../../themes';
+import { Icon } from '../../assets';
+
+interface InputSearchProps {
+  onSearch: (text: string) => void;
+  onSortPress: () => void;
+  sortText: string;
+}
+const Searchbar = ({ onSearch, onSortPress, sortText }: InputSearchProps) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Icon.Search width={24} height={24} />
+        <TextInput
+          placeholder="Cari nama, bank, atau nominal"
+          placeholderTextColor={COLORS.darkGrey}
+          style={styles.input}
+          onChangeText={onSearch}
+        />
+      </View>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.containerFilter}
+        onPress={onSortPress}>
+        <Text style={styles.textFilter}>{sortText || 'URUTKAN'}</Text>
+        <Icon.ChevronDown />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Searchbar;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    marginHorizontal: 8,
+    marginTop: 16,
+    marginBottom: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 3,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333333',
+    marginLeft: 4,
+  },
+  containerFilter: { flexDirection: 'row', alignItems: 'center' },
+  textFilter: { color: COLORS.tomato, fontWeight: 'bold' },
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import ErrorAPI from './Popups/ErrorAPI';
+import Searchbar from './Inputs/Searchbar';
 import TransactionItem from './Cards/TransactionItem';
 import TransferStatus from './Labels/TransferStatus';
 
-export { ErrorAPI, TransactionItem, TransferStatus };
+export { ErrorAPI, Searchbar, TransactionItem, TransferStatus };
