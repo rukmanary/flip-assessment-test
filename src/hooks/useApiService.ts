84e607b908ae4dd484b830468c8f69a328d84430
ryan <rukmanary@gmail.com>
2024-11-19 00:12:35 +0700

    modify userApiService hooks

--- src/hooks/useApiService.ts
+++ src/hooks/useApiService.ts
@@ -2,17 +2,23 @@ import { useState, useCallback } from 'react';
 
 interface UseApiServiceReturn<T> {
   data: T | null;
+  rawData: T | null;
   loading: boolean;
   error: string | null;
   statusCode: number | null;
   fetchAPI: (...args: any[]) => Promise<void>;
   applyFilter: (filterFn: (data: T) => T) => void;
+  search: (
+    query: string,
+    searchFn: (item: T, query: string) => boolean,
+  ) => void;
 }
 
 const useApiService = <T>(
   apiFunction: (...args: any[]) => Promise<T>,
 ): UseApiServiceReturn<T> => {
   const [data, setData] = useState<T | null>(null);
+  const [rawData, setRawData] = useState<T | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<any>(null);
   const [statusCode, setStatusCode] = useState<number | null>(null);
@@ -27,6 +33,7 @@ const useApiService = <T>(
       try {
         const result = await apiFunction(...fetchAPIArgs);
         setData(result);
+        setRawData(result);
       } catch (err: any) {
         console.log({ err: err.response });
         setError(err.response?.data?.message || err.message || 'Unknown error');
@@ -38,16 +45,44 @@ const useApiService = <T>(
     [apiFunction],
   );
 
-  const applyFilter = useCallback(
-    (filterFn: (data: T) => T) => {
-      if (data) {
-        setData(filterFn(data));
+  const search = useCallback(
+    (query: string, searchFunction: (item: T, query: string) => boolean) => {
+      if (!query) {
+        setData(rawData);
+        return;
+      }
+
+      if (Array.isArray(rawData)) {
+        const lowerCaseQuery = query.toLowerCase();
+        const filtered = rawData?.filter?.(item =>
+          searchFunction(item, lowerCaseQuery),
+        );
+        setData(filtered as T);
       }
     },
-    [data],
+    [rawData],
   );
 
-  return { data, loading, error, fetchAPI, applyFilter, statusCode };
+  const applyFilter = useCallback(
+    (filterFn: (data: T) => T) => {
+      if (rawData) {
+        const filtered = filterFn(rawData);
+        setData(filtered);
+      }
+    },
+    [rawData],
+  );
+
+  return {
+    data,
+    rawData,
+    loading,
+    error,
+    fetchAPI,
+    applyFilter,
+    statusCode,
+    search,
+  };
 };
 
 export default useApiService;
