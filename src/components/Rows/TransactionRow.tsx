ae5da0b68f79382a1c8890640581934693ffa223
ryan <rukmanary@gmail.com>
2024-11-18 16:19:51 +0700

    add component TransactionRow

---
+++ src/components/Rows/TransactionRow.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TransactionRowProps } from '../../types';
+
+const TransactionRow = ({
+  leftTitle = '',
+  leftValue = '',
+  rightTitle = '',
+  rightValue = '',
+}: TransactionRowProps) => {
+  return (
+    <View style={styles.row}>
+      <View style={styles.flex1}>
+        <Text style={styles.title}>{leftTitle}</Text>
+        <Text style={styles.value}>{leftValue}</Text>
+      </View>
+
+      {rightTitle && (
+        <View style={styles.flex2}>
+          <Text style={styles.title}>{rightTitle}</Text>
+          <Text style={styles.value}>{rightValue}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: 16,
+  },
+  flex1: { flex: 1 },
+  flex2: { flex: 0.7 },
+  title: { fontWeight: '600', marginBottom: 4 },
+  value: { fontWeight: '400', fontSize: 16 },
+});
+
+export default TransactionRow;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import Searchbar from './Inputs/Searchbar';
 import TransactionItem from './Cards/TransactionItem';
 import TransferStatus from './Labels/TransferStatus';
 import BankTransferRoute from './Labels/BankTransferRoute';
+import TransactionRow from './Rows/TransactionRow';
 
 export {
   ErrorAPI,
@@ -10,4 +11,5 @@ export {
   TransactionItem,
   TransferStatus,
   BankTransferRoute,
+  TransactionRow,
 };
