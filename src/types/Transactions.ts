d9e45f37c65b2ae2c22f661fe6bcc6474e5b6121
ryan <rukmanary@gmail.com>
2024-11-18 12:12:04 +0700

    add types

---
+++ src/types/Transactions.ts
@@ -0,0 +1,16 @@
+export interface DetailTransactionData {
+  id: string;
+  amount: number;
+  unique_code: number;
+  status: string;
+  sender_bank: string;
+  account_number: string;
+  beneficiary_name: string;
+  beneficiary_bank: string;
+  remark: string;
+  created_at: string;
+  completed_at: string;
+  fee: number;
+}
+
+export type TransactionListData = DetailTransactionData[];
